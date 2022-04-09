9215cfb38f4e6ad1d9c67992169ee0433e5f4b5d
İlyas Bozdemir <52322835+ilyasBozdemir@users.noreply.github.com>
2022-04-09 21:31:00 +0300

    Add files via upload

--- csharp/diziler_09.cs
+++ csharp/diziler_09.cs
@@ -3,41 +3,23 @@ class diziler_09
 {
     public static async void Main()
     {
-            int[] sayiDizisi = {14,102,90,70,35,15,17,4};
-            Console.WriteLine("Sırasız Dizi");
-            foreach (var sayi in sayiDizisi)
-            {
-                Console.WriteLine(sayi);
-            }
-            Console.WriteLine("Sıralı Dizi");
-            Array.Sort(sayiDizisi);
-            foreach (var sayi in sayiDizisi)
-            {
-                Console.WriteLine(sayi);
-            }
-            Console.WriteLine("Array Clear");
-            Array.Clear(sayiDizisi,2,2);
-            foreach (var sayi in sayiDizisi)
-            {
-                Console.WriteLine(sayi);
-            }
-
-            Console.WriteLine("Array Reverse");
-            Array.Reverse(sayiDizisi);
-            foreach (var sayi in sayiDizisi)
-            {
-                Console.WriteLine(sayi);
-            }
-
-            Console.WriteLine("Array Indexof");
-            
-            Console.WriteLine(Array.IndexOf(sayiDizisi,23));
-            Console.WriteLine("Array Resize");
-            Array.Resize<int>(ref sayiDizisi,9);
-            sayiDizisi[8] =99;
-            foreach (var sayi in sayiDizisi)
-            {
-                Console.WriteLine(sayi);
-            }
+            int[] sayilar = new int[10];
+            sayilar[2] = 12;
+            string[] renkler = {"Mavi","Kırmızı","Sarı","Yeşil"};
+            int[] dizi;
+            dizi = new int[4];
+            string[] renkler = {"Mavi","Kırmızı","Sarı","Yeşil"};
+            string renk = renkler[1]; // Kırmızı rengi getirir.
+            int[] sayiDizisi = new int[20];
+            int toplam = 0;
+            for (int i = 0; i < sayiDizisi.Length; i++)
+             {
+                 Console.Write("Lütfen {0}. sayıyı girin:", i+1);
+                 sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+                 toplam += sayiDizisi[i];
+                 }
+                 double ortalama = toplam / 20;
+                 Console.WriteLine("Sayıların ortalaması: " + ortalama);
+                 Console.ReadLine();
     }
 }
