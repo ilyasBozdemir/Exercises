312c56d57e0f4c05d1d48a46d79c17491710dea8
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-18 17:23:43 +0300

    soru cevapları

--- Koleksiyonlarla-ilgili-algoritma-sorulari/Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Program.cs
+++ Koleksiyonlarla-ilgili-algoritma-sorulari/Koleksiyonlar-Soru-3/Koleksiyonlar-Soru-3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections;
+
+ArrayList vowelsArray = new ArrayList();
+string vowels = "aeıioöuü";
+Console.Write("Cümle girin : ");
+var data = Console.ReadLine();
+
+for (int i = 0; i < data.Length; i++)
+    if (vowels.Contains(data[i]))
+        vowelsArray.Add(data[i]);
+Console.WriteLine("Sesli Harfler :");
+foreach (var item in vowelsArray)
+    Console.Write($"{item},");
+
+
