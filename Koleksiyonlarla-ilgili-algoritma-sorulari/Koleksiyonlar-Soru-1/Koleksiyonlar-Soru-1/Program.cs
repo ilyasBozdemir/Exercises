989fba5cb5dad4431af709434a3bf1100fa1020a
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-18 16:40:16 +0300

    project added.

---
+++ Koleksiyonlarla-ilgili-algoritma-sorulari/Koleksiyonlar-Soru-1/Koleksiyonlar-Soru-1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+ArrayList arrayList = new ArrayList();
+ArrayList primeNumberList = new ArrayList();
+ArrayList nonPrimeNumberList = new ArrayList();
+int index = 0, sayi, ortalama = 0, val = 20;
+Console.WriteLine($"{val} adet pozitif sayı girişi yapın");
+while (index < val)
+{
+    Console.Write($"{index + 1}. sayıyı girin:");
+    bool state = 
+        int.TryParse(Console.ReadLine(), out sayi);
+    if (state && sayi > 0)//int ve pozitif sayi olursa 
+    {
+        bool isPrimeNumber = SayiAsalMi(sayi);
+        if (isPrimeNumber)
+            primeNumberList.Add(sayi);
+        else
+            nonPrimeNumberList.Add(sayi);
+        arrayList.Add(sayi);
+        index++;
+    }
+}
+Console.Write($"\nGirilen sayılar: ");
+foreach (var item in arrayList)
+    Console.Write($"{item} ");
+
+primeNumberList.Sort();//küçükten büyüge
+primeNumberList.Reverse();//tam tersi büyükten küçüğe
+
+Console.WriteLine($"\nAsal sayılar;");
+foreach (var item in primeNumberList)
+{ 
+    Console.Write($"{item} ");
+    ortalama += int.Parse(item + "");
+}
+ortalama = (ortalama / primeNumberList.Count);
+Console.WriteLine($"dizi adedi:{primeNumberList.Count} ortalaması : {ortalama}");
+nonPrimeNumberList.Sort();//küçükten büyüge
+nonPrimeNumberList.Reverse();//tam tersi büyükten küçüğe
+Console.WriteLine("------------------");
+Console.WriteLine($"\nAsal olmayan sayılar;");
+foreach (var item in nonPrimeNumberList)
+{
+    Console.Write($"{item} ");
+    ortalama += int.Parse(item + "");
+}
+ortalama = (ortalama / nonPrimeNumberList.Count);
+Console.WriteLine($"dizi adedi:{nonPrimeNumberList.Count} ortalaması : {ortalama}");
+
+bool SayiAsalMi(int sayi)
+{
+    int kontrol = 0;
+    for (int i = 2; i < sayi; i++)
+        if (sayi % i == 0)
+        {
+            kontrol = 1;
+            break;
+        }
+    if (kontrol != 1)
+        return true;
+    return false;
+}
+
+Console.Read();
