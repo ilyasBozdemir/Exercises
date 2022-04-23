c0819bf8cc42dda6270c26419897878461de3697
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-23 18:18:51 +0300

    created todoapp

--- todo-app/todo-app/Program.cs
+++ todo-app/todo-app/Program.cs
@@ -1,1 +1,37 @@
 ﻿
+string[] ozellikler = { "Board Listelemek", "Board'a Kart Eklemek", "Board'dan Kart Silmek", "Kart Taşımak" };
+anaSecim: 
+byte sayac = 0;
+Console.WriteLine("Lütfen yapmak istediğiniz işlemi seçiniz :)");
+Console.WriteLine("*******************************************");
+foreach (var ozellik in ozellikler)
+{
+    sayac++;
+    Console.WriteLine($"({sayac}) {ozellik}");
+}
+int secim;
+int.TryParse(Console.ReadLine(), out secim);
+todo_app.TodoOperations todo = new todo_app.TodoOperations();
+switch (secim)
+{
+    case 1:
+        Console.Clear();
+        todo.BoardListele();
+        break;
+    case 2:
+        Console.Clear();
+        todo.BoardaKartEkle();
+        break;
+    case 3:
+        Console.Clear();
+        todo.BoarddanKartSil();
+        break; 
+    case 4:
+        Console.Clear();
+        todo.KartTasi();
+        break;
+    default:Console.Clear();
+        Console.WriteLine("Menüden Seçim Yapınız.");
+        goto anaSecim;
+        break;
+}
---
+++ todo-app/todo-app/TodoOperations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo_app
+{
+
+    public class TodoOperations
+    {
+        public void BoardListele()
+        {
+            
+        }
+        public void BoardaKartEkle()
+        {
+            
+        }
+        public void BoarddanKartSil()
+        {
+            
+        }
+        public void KartTasi()
+        {
+            
+        }
+    }
+
+}
