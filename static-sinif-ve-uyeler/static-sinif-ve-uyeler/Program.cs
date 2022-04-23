a643bb80c0d112bd1ee9b172feb9f868310cbdc4
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-23 10:49:21 +0300

    project added.

---
+++ static-sinif-ve-uyeler/static-sinif-ve-uyeler/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine($"Öğrenci sayısı: {Ogrenci.OgrenciSayisi}");
+
+Ogrenci ogrenci1 = new Ogrenci();
+ogrenci1.Isim = "Deniz";
+ogrenci1.Soyisim = "Arda";
+
+Ogrenci ogrenci2 = new Ogrenci();
+ogrenci2.Isim = "Ayşe";
+ogrenci2.Soyisim = "Yılmaz";
+
+Console.WriteLine($"Öğrenci sayısı: {Ogrenci.OgrenciSayisi}");
+
+class Ogrenci
+{
+    public static int OgrenciSayisi = 0;
+    public string Isim;
+    public string Soyisim;
+    public Ogrenci()
+    {
+        OgrenciSayisi += 1;
+    }
+}
