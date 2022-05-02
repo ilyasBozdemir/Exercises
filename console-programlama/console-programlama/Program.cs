2834c62c0838ed9f8368692816977501ebeb737a
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-05-02 12:55:22 +0300

    create console-programlama

---
+++ console-programlama/console-programlama/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace console_programlama
+{
+    public static class Program
+    {
+        static void Main()
+        {
+            Console.Write("İsminizi giriniz: ");
+            string isim = Console.ReadLine();
+
+            Console.WriteLine($"Merhaba {isim}");
+            Console.Read();
+        }
+    }
+}
